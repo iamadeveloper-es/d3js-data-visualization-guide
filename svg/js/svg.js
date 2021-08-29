77affab2a7a1e41d343be6362f4655072339422b
Daniel Verdú <verdudaniel7@gmail.com>
2021-08-29 13:10:13 +0200

    line with svg

--- svg/js/svg.js
+++ svg/js/svg.js
@@ -5,17 +5,27 @@ const svg = canvas.append("svg")
             .attr("width", 100)
             .attr("height", 100)
 
+//circle
 const circle = svg.append("circle")
                 .attr("cx", 50)
                 .attr("cy", 50)
                 .attr("r", 50)
                 .attr("fill", "red")
 
+//rect
 const rect = svg.append("rect")
                 .attr("width", 25)
                 .attr("height", 25)
                 .attr("x", (100/2) - 12.5)
                 .attr("y", (100/2) - 12.5)
-                .style('transform', "rotate(45deg)" )
-                .style('transform-origin', "center" )
+                //.style('transform', "rotate(45deg)"50
+                //.style('transform-origin', "center" )
 
+//line
+const line = svg.append("line")
+                .attr("x1", 0)
+                .attr("y1", 0)
+                .attr("x2", 100)
+                .attr("y2", 100)
+                .attr("stroke-width", 2)
+                .attr("stroke", "black")
